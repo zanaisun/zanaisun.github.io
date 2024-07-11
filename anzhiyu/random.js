e29cf72881cda3dfad17795278a7658b4cd2475b
zanaisun <2966337487@qq.com>
2024-07-12 03:10:44 +0800

    Site updated: 2024-07-12 03:10:44

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/7a776471.html","posts/4a17b156.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/4a17b156.html","posts/7a776471.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
